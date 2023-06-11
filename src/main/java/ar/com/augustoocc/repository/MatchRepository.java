6449cc98ab9c245f46f25cd8d66a22accc2744b8
augustoocch <augusto.occh@gmail.com>
2023-06-11 13:47:27 -0300

    Added reactive libraries

--- src/main/java/ar/com/augustoocc/repository/MatchRepository.java
+++ src/main/java/ar/com/augustoocc/repository/MatchRepository.java
@@ -1,7 +1,6 @@
 package ar.com.augustoocc.repository;
 
 import ar.com.augustoocc.model.Match;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 
